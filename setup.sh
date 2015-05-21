6828b3fd97a732c6ca5f9a07c2f71321b63e3345
JinSung Kang <gogs14@hotmail.com>
2015-05-21 17:15:28 -0400

    adding all the plugins

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# copy the vimrc to main
+cp .vimrc_plugin ~/.vimrc
+
+#install Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#install plugins
+cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-fugitive
+git clone https://github.com/majutsushi/tagbar
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/jistr/vim-nerdtree-tabs.git
+git clone https://github.com/tpope/vim-surround
+git clone https://github.com/altercation/vim-colors-solarized
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/scrooloose/nerdcommenter
+git clone https://github.com/ervandew/supertab
+git clone https://github.com/tpope/vim-repeat
+
+mkdir -p ~/.vim/after/syntax/
+git clone https://github.com/octol/vim-cpp-enhanced-highlight.git ~/.vim/after/syntax
